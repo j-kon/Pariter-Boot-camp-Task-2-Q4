7010b9977b120c375a2cecdd66ddd2fafe11cf47
Jeremiah Jacob <jaykon4me@gmail.com>
2019-01-11 04:32:38 +0100

    Add files via upload

---
+++ Q4 of Task2.java
@@ -0,0 +1,7 @@
+import java.util.Scanner;
+
+Scanner reader = new Scanner(System.in);  // Reading from System.in
+System.out.println("Enter a number: ");
+int n = reader.nextInt(); // Scans the next token of the input as an int.
+//once finished
+reader.close();
